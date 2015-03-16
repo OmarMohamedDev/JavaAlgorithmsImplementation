68d2771cc71d6cc16aec257b07e77555ca2bbcf4
omar <arahell55@gmail.com>
2015-03-16 18:38:06 +0100

    Exercise 1 completed

---
+++ src/mohamed/esercizio1/IntSortedArrayTest.java
@@ -0,0 +1,19 @@
+package mohamed.esercizio1;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class IntSortedArrayTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
