dc6652c8d685ef95ac3b55eff871ea52bedf3bfe
omar <arahell55@gmail.com>
2015-05-22 17:41:02 +0200

    Working on the test cases of the classes of the priorityqueue exercise

--- mohamed/Java/mohamed/priorityqueue/PriorityQueueStringDoubleHeapTest.java
+++ mohamed/Java/mohamed/priorityqueue/PriorityQueueStringDoubleHeapTest.java
@@ -2,7 +2,8 @@ package mohamed.priorityqueue;
 
 import org.junit.Test; 
 import org.junit.Before; 
-import org.junit.After; 
+import org.junit.After;
+import static org.junit.Assert.*;
 
 /** 
 * PriorityQueueStringDoubleHeap Tester. 
@@ -11,73 +12,194 @@ import org.junit.After;
 */ 
 public class PriorityQueueStringDoubleHeapTest { 
 
-@Before
-public void before() throws Exception { 
-} 
+    @Before
+    public void before() throws Exception {
+    }
 
-@After
-public void after() throws Exception { 
-} 
+    @After
+    public void after() throws Exception {
+    }
 
-/** 
-* 
-* Method: add(String element, double priority) 
-* 
-*/ 
-@Test
-public void testAdd() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+     * Test of isEmpty method, of class PriorityQueueStringDoubleHeap.
+     */
+    @Test
+    public void testisEmptyNullParameters() {
+        try {
+            PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(null, null);
+            assertTrue(false);
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
 
-/** 
-* 
-* Method: first() 
-* 
-*/ 
-@Test
-public void testFirst() throws Exception { 
-//TODO: Test goes here... 
-} 
+    }
 
-/** 
-* 
-* Method: removeFirst() 
-* 
-*/ 
-@Test
-public void testRemoveFirst() throws Exception { 
-//TODO: Test goes here... 
-} 
+    @Test
+    public void testisEmptyNotEmpty() {
+        String[] elem = {"first"};
+        double[] prior = {0.0};
 
-/** 
-* 
-* Method: isEmpty() 
-* 
-*/ 
-@Test
-public void testIsEmpty() throws Exception { 
-//TODO: Test goes here... 
-} 
+        PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(elem, prior);
+        assertFalse(h.isEmpty());
+    }
 
-/** 
-* 
-* Method: delete(String element) 
-* 
-*/ 
-@Test
-public void testDelete() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+     * Test of Add method, of class PriorityQueueStringDoubleHeap.
+     */
+    @Test
+    public void testAddOneElementOneSpot() {
+        PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(1);
+        String elem = "first";
+        double prior = 0.0;
+        try {
+            h.add(elem, prior);
+            assertTrue(true);
+        } catch (IllegalArgumentException e) {
+            assertTrue(false);
+        }
 
-/** 
-* 
-* Method: setPriority(String element, double priority) 
-* 
-*/ 
-@Test
-public void testSetPriority() throws Exception { 
-//TODO: Test goes here... 
-} 
+    }
+
+    @Test
+    public void testAddTwoElementsOneSpot() {
+        PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(1);
+        String elem = "second";
+        double prior = 1.0;
+
+        try {
+            h.add(elem, prior);
+            elem = "first";
+            prior = 0.0;
+            h.add(elem, prior);
+            assertTrue(false);
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+
+    }
+
+    @Test
+    public void testAddTwoElementsTwoSpots() {
+        PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(2);
+        String elem = "second";
+        double prior = 1.0;
+
+        try {
+            h.add(elem, prior);
+            elem = "first";
+            prior = 0.0;
+            h.add(elem, prior);
+            assertEquals("first", h.first());
+        } catch (IllegalArgumentException e) {
+            assertTrue(false);
+        }
+    }
+
+    /**
+     * Test of first method, of class PriorityQueueStringDoubleHeap.
+     */
+    @Test
+    public void testFirstOneElement() {
+        String[] elem = {"first"};
+        double[] prior = {0.0};
+        PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(elem, prior);
+        assertEquals("first", h.first());
+    }
+
+    @Test
+    public void testFirstTwoElements() {
+        String[] elem = {"second", "first"};
+        double[] prior = {1.0, 0.0};
+        PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(elem, prior);
+        assertEquals("first", h.first());
+    }
+
+    /**
+     * Test of removeFirst method, of class PriorityQueueStringDoubleHeap.
+     */
+    @Test
+    public void testRemoveFirstTwoElementsOneTime() {
+        String[] elem = {"second", "first"};
+        double[] prior = {1.0, 0.0};
+        PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(elem, prior);
+        assertEquals("first", h.removeFirst());
+    }
+
+    @Test
+    public void testRemoveFirstTwoElementsTwice() {
+        String[] elem = {"second", "first"};
+        double[] prior = {1.0, 0.0};
+        PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(elem, prior);
+        assertEquals("first", h.removeFirst());
+        assertEquals("second", h.removeFirst());
+    }
+
+    @Test
+    public void testRemoveFirstTwoElementsThreeTimes() {
+        String[] elem = {"second", "first"};
+        double[] prior = {1.0, 0.0};
+        PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(elem, prior);
+        assertEquals("first", h.removeFirst());
+        assertEquals("second", h.removeFirst());
+        try {
+            h.removeFirst();
+            assertTrue(false);
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+
+    }
+
+    @Test
+    public void testRemoveFirstThreeElementsThreeTimes() {
+        String[] elem = {"second", "third", "first"};
+        double[] prior = {1.0, 1.5, 0.0};
+        PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(elem, prior);
+        assertEquals("first", h.removeFirst());
+        assertEquals("second", h.removeFirst());
+        assertEquals("third", h.removeFirst());
+    }
+
+    /**
+     * Test of setPriority method, of class PriorityQueueStringDoubleHeap.
+     */
+    @Test
+    public void testSetPriority() {
+        PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(3);
+        h.add("third", 3);
+        h.add("second", 2);
+        h.add("first", 1);
+        assertEquals("first", h.first());
+        h.setPriority("third", 0);
+        assertEquals("third", h.first());
+
+    }
+
+    @Test
+    public void testSetPriorityOtherConstructor() {
+        String[] elem = {"second", "third", "first"};
+        double[] prior = {2, 3, 1};
+        PriorityQueueStringDoubleHeap h = new PriorityQueueStringDoubleHeap(elem, prior);
+        assertEquals("first", h.first());
+        h.setPriority("third", 0);
+        assertEquals("third", h.first());
+
+    }
+
+    /**
+     * Test of delete method, of class PriorityQueueStringDoubleHeap.
+     */
+    @Test
+    public void testDeleteEmptyQueue() {
+
+
+    }
+
+    @Test
+    public void testDeleteNotEmptyQueue() {
+
+
+    }
 
 
 } 
