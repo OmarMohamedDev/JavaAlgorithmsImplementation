5bee7949c7358462e832b739b4661f95a2c7d05e
omar <arahell55@gmail.com>
2015-05-26 20:26:51 +0200

    Created basic searchtree without AVL balancing specifications

--- mohamed/Java/mohamed/dictionary/AVLTree.java
+++ mohamed/Java/mohamed/dictionary/AVLTree.java
@@ -1,9 +1,30 @@
 package mohamed.dictionary;
 
 /**
- * Created by omar on 5/25/15.
+ * @author Omar Mohamed
  */
-public class AVLTree implements SortedDictionary<Integer,String> {
+public class AVLTree implements SortedDictionary<Integer,Integer> {
+
+    /**
+     * Root of the tree
+     */
+    private Node root;
+
+    /**
+     * Public constructor that set the root to null
+     */
+    public AVLTree(){
+        this.root = null;
+    }
+
+    /**
+     * Public constructor that permit to initialize the root of the tree
+     * @param root
+     */
+    public AVLTree(Node root){
+        this.root = root;
+    }
+
     /**
      * Returns the minimun key in the sorted dictionary
      *
@@ -27,7 +48,7 @@ public class AVLTree implements SortedDictionary<Integer,String> {
      *
      * @return the element associated with the minimum key
      */
-    public String ElementOfMinKey() {
+    public Integer ElementOfMinKey() {
         return null;
     }
 
@@ -36,7 +57,7 @@ public class AVLTree implements SortedDictionary<Integer,String> {
      *
      * @return the element associated with the maxixum key
      */
-    public String ElementOfMaxKey() {
+    public Integer ElementOfMaxKey() {
         return null;
     }
 
@@ -46,7 +67,7 @@ public class AVLTree implements SortedDictionary<Integer,String> {
      * @return true if the dictionary is empty, false otherwise
      */
     public boolean isEmpty() {
-        return false;
+        return root == null;
     }
 
     /**
@@ -55,8 +76,8 @@ public class AVLTree implements SortedDictionary<Integer,String> {
      * @param key the key of the element that we want to search
      * @return the value associated to the key, if it's present. Null otherwise
      */
-    public String find(Integer key) {
-        return null;
+    public Integer find(Integer key) {
+        return root == null ? null : root.find(key);
     }
 
     /**
@@ -66,8 +87,13 @@ public class AVLTree implements SortedDictionary<Integer,String> {
      * @param value value that we want to associate to the key
      * @return the previous value associated to the key, if any. Null otherwise
      */
-    public String add(Integer key, String value) {
-        return null;
+    public Integer add(Integer key, Integer value) {
+        if(root == null){
+            root = new Node(key, value);
+            return null;
+        }
+        else
+            return root.add(key, value);
     }
 
     /**
@@ -76,6 +102,140 @@ public class AVLTree implements SortedDictionary<Integer,String> {
      * @param key key that we want to remove
      */
     public void remove(Integer key) {
+        root = remove(key, root);
+    }
+
+    /**
+     * Auxiliary method that returns the node with the minimum value in the tree
+     * @return the node with the minimum value int the tree
+     */
+    protected static Node minValue(Node node){
+        while(node.left != null) node = node.left;
+        return node;
+    }
+
+    /**
+     * Auxiliary method used to delete the minimum value inside the tree
+     * @param node current node that we are checking
+     * @return root of the tree without the minimum element
+     */
+    protected static Node deleteMinValue(Node node){
+        if(node.left == null) return node.right;
+        node.left = deleteMinValue(node.left);
+        return node;
+    }
+
+    /**
+     * Internal method that implements the remove inside the tree
+     * @param key key of the element that we want to remove
+     * @param node root of the tree where we want to operate
+     * @return the root of the tree without the node with key "key", if was present
+     */
+    protected static Node remove(int key, Node node){
+        if(node == null) return null;
+        int nodeKey = node.getKey();
+        if(key < nodeKey) node.left = remove(key, node.left);
+        else if(key > nodeKey) node.right = remove(key, node.right);
+        else{
+            if(node.left == null) node = node.right;
+            else if(node.right == null) node = node.left;
+            else{
+                Node minimumNode = minValue(node.right);
+                node.value = minimumNode.getValue();
+                node.key = minimumNode.getKey();
+                node.right = deleteMinValue(node.right);
+            }
+        }
+        return node;
+    }
+
+
+
+    /**
+     * Inner class that represent the nodes of the tree
+     */
+    private class Node {
+
+        Node left;
+        Node right;
+        Integer value;
+        Integer key;
+        int height;
+
+        Node() {
+            key = null;
+            value = null;
+            left = right = null;
+            int height = 0;
+        }
+
+        Node(Integer key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        Integer getKey(){
+            return key;
+        }
+
+        Integer getValue(){
+            return value;
+        }
+
+        /**
+         * Return the height of a node
+         * @return height of the node
+         */
+        int height(){
+            return height;
+        }
+
+        /**
+         * Recursive method that search in the tree an element with key "key"
+         * @param key the key of the element that we are looking for
+         * @return the value of the element with key "key", if present. Null otherwise
+         */
+        Integer find(Integer key){
+            int thisKey = getKey();
+            if(key < thisKey) return left == null ? null : left.find(key);
+            else if(key > thisKey) return right == null ? null : right.find(key);
+            else return value;
+        }
+
+        /**
+         * Associate the value of "value" to the key "key" in the dictionary
+         *
+         * @param key   key that we want to associate to the value
+         * @param value value that we want to associate to the key
+         * @return the previous value associated to the key, if any. Null otherwise
+         */
+        Integer add(Integer key, Integer value){
+            int thisKey = getKey();
+            Integer oldValue = null;
+
+            if(key < thisKey) {
+                if (left == null){
+                    left = new Node(key, value);
+                    return oldValue;
+                }
+                else
+                    return left.add(key, value);
+            }
+            else if(key > thisKey){
+                if (right == null){
+                    right = new Node(key, value);
+                    return oldValue;
+                }
+                else
+                    return right.add(key, value);
+            }
+            else{
+                oldValue = this.getValue();
+                this.value = value;
+                return oldValue;
+            }
+        }
+
 
     }
 }
--- mohamed/Java/mohamed/dictionary/AVLTreeTest.java
+++ mohamed/Java/mohamed/dictionary/AVLTreeTest.java
@@ -2,104 +2,448 @@ package mohamed.dictionary;
 
 import org.junit.Test; 
 import org.junit.Before; 
-import org.junit.After; 
+import org.junit.After;
+import static org.junit.Assert.*;
 
 /** 
 * AVLTree Tester. 
 * 
-* @author <Authors name> 
-* @since <pre>May 25, 2015</pre> 
-* @version 1.0 
+* @author Omar Mohamed
 */ 
-public class AVLTreeTest { 
+public class AVLTreeTest {
 
-@Before
-public void before() throws Exception { 
-} 
+    AVLTree avlTree;
 
-@After
-public void after() throws Exception { 
-} 
 
-/** 
-* 
-* Method: minKey() 
-* 
-*/ 
-@Test
-public void testMinKey() throws Exception { 
-//TODO: Test goes here... 
-} 
 
-/** 
-* 
-* Method: maxKey() 
-* 
-*/ 
-@Test
-public void testMaxKey() throws Exception { 
-//TODO: Test goes here... 
-} 
+    @Before
+    public void setUp() throws Exception {
+        avlTree = new AVLTree();
+    }
 
-/** 
-* 
-* Method: ElementOfMinKey() 
-* 
-*/ 
-@Test
-public void testElementOfMinKey() throws Exception { 
-//TODO: Test goes here... 
-} 
+    @After
+    public void tearDown() throws Exception {
 
-/** 
-* 
-* Method: ElementOfMaxKey() 
-* 
-*/ 
-@Test
-public void testElementOfMaxKey() throws Exception { 
-//TODO: Test goes here... 
-} 
+    }
 
-/** 
-* 
-* Method: isEmpty() 
-* 
-*/ 
-@Test
-public void testIsEmpty() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+    *
+    * Method: minKey()
+    *
+    */
+    @Test
+    public void testMinKey() throws Exception {
+    //TODO: Test goes here...
+    }
 
-/** 
-* 
-* Method: find(Integer key) 
-* 
-*/ 
-@Test
-public void testFind() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+    *
+    * Method: maxKey()
+    *
+    */
+    @Test
+    public void testMaxKey() throws Exception {
+    //TODO: Test goes here...
+    }
 
-/** 
-* 
-* Method: add(Integer key, String value) 
-* 
-*/ 
-@Test
-public void testAdd() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+    *
+    * Method: ElementOfMinKey()
+    *
+    */
+    @Test
+    public void testElementOfMinKey() throws Exception {
+    //TODO: Test goes here...
+    }
 
-/** 
-* 
-* Method: remove(Integer key) 
-* 
-*/ 
-@Test
-public void testRemove() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+    *
+    * Method: ElementOfMaxKey()
+    *
+    */
+    @Test
+    public void testElementOfMaxKey() throws Exception {
+    //TODO: Test goes here...
+    }
+
+    /**
+     * Test of remove method, of class AVLTree.
+     */
+
+    @Test
+    public void testRemoveNoNodes() {
+
+        Integer key=1;
+        avlTree.remove(key);
+        assertNull(avlTree.find(1));
+    }
+
+    @Test
+    public void testRemoveKeyDoesntExistAndOneNode() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        avlTree.add(key, value);
+
+        key=1;
+        avlTree.remove(key);
+        assertNull(avlTree.find(1));
+    }
+
+
+    @Test
+    public void testRemoveKeyExistsAndOneNode() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        avlTree.add(key, value);
+
+        key=5;
+        Integer expResult= null;
+        avlTree.remove(key);
+        assertEquals(expResult, avlTree.find(5));
+    }
+
+    @Test
+    public void testRemoveKeyExistsFirstTime() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        avlTree.add(key, value);
+
+        key=5;
+        Integer expResult= null;
+        avlTree.remove(key);
+        assertEquals(expResult, avlTree.find(5));
+
+        expResult=null;
+        avlTree.remove(key);
+        assertEquals(expResult, avlTree.find(5));
+    }
+
+    @Test
+    public void testRemoveKeyExistsAndHasAChild() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        avlTree.add(key, value);
+
+        key = 4;
+        value = 3;
+        avlTree.add(key, value);
+
+        key=5;
+        Integer expResult= null;
+        avlTree.remove(key);
+        assertEquals(expResult, avlTree.find(5));
+    }
+
+
+    @Test
+    public void testRemoveKeyExistsAndHasTwoChildren() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        avlTree.add(key, value);
+
+        key = 4;
+        value = 3;
+        avlTree.add(key, value);
+
+        key = 6;
+        value = 2;
+        avlTree.add(key, value);
+
+        key = 5;
+        Integer expResult= null;
+        avlTree.remove(key);
+        assertEquals(expResult, avlTree.find(5));
+    }
+
+    @Test
+    public void testRemoveKeyExistsAndIsNotRoot() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        avlTree.add(key, value);
+
+        key = 4;
+        value = 3;
+        avlTree.add(key, value);
+
+        key = 6;
+        value = 6;
+        avlTree.add(key, value);
+
+        key = 4;
+        value = 3;
+        avlTree.add(key, value);
+
+        key = 7;
+        value = 4;
+        avlTree.add(key, value);
+
+        key = 6;
+        Integer expResult= null;
+        avlTree.remove(key);
+        assertEquals(expResult, avlTree.find(6));
+    }
+
+    @Test
+    public void testRemoveRemoveEachNode() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        avlTree.add(key, value);
+
+        key = 4;
+        value = 3;
+        avlTree.add(key, value);
+
+        key = 6;
+        value = 6;
+        avlTree.add(key, value);
+
+        key = 3;
+        value = 4;
+        avlTree.add(key, value);
+
+        key = 7;
+        value = 4;
+        avlTree.add(key, value);
+
+        key=6;
+        Integer expResult= null;
+        avlTree.remove(key);
+        assertEquals(expResult, avlTree.find(6));
+
+        key=5;
+        expResult= null;
+        avlTree.remove(key);
+        assertEquals(expResult, avlTree.find(5));
+
+        key=4;
+        expResult= null;
+        avlTree.remove(key);
+        assertEquals(expResult, avlTree.find(4));
+
+        key=7;
+        expResult= null;
+        avlTree.remove(key);
+        assertEquals(expResult, avlTree.find(7));
+
+        key=3;
+        expResult= null;
+        avlTree.remove(key);
+        assertEquals(expResult, avlTree.find(3));
+
+    }
+
+
+    /**
+     * Test of find method, of class AVLTree.
+     */
+
+    @Test
+    public void testFindNoNodes() {
+        avlTree = new AVLTree();
+
+        Integer key=1;
+        Integer expResult=null;
+        Integer result = avlTree.find(key);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testFindOneNode() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        avlTree.add(key, value);
+
+        key=5;
+        Integer expResult=1;
+        Integer result = avlTree.find(key);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testFindOneNodeFindTwice() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        avlTree.add(key, value);
+
+        //key=5;
+        Integer expResult=1;
+        Integer result = avlTree.find(key);
+        assertEquals(expResult, result);
+        result = avlTree.find(key);
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testFindFindEachOne() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        avlTree.add(key, value);
+
+        key=4;
+        value=2;
+        avlTree.add(key, value);
+
+        key=6;
+        value=3;
+        avlTree.add(key, value);
+
+        key=5;
+        Integer expResult=1;
+        Integer result = avlTree.find(key);
+        assertEquals(expResult, result);
+
+        key=4;
+        expResult=2;
+        result = avlTree.find(key);
+        assertEquals(expResult, result);
+
+        key=6;
+        expResult=3;
+        result = avlTree.find(key);
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of add method, of class AVLTree.
+     */
+    @Test
+    public void testAddFirstElem() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        Integer expResult = null;
+        Integer result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testAddTwoElementsSameKey() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        Integer expResult = null;
+        Integer result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+
+        key = 5;
+        value = 2;
+        expResult=1;
+        result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testAddTwoElementsDifferentKey() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        Integer expResult = null;
+        Integer result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+
+        key = 6;
+        value = 2;
+        expResult=null;
+        result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testAddThreeElementsTwoKeysRight() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        Integer expResult = null;
+        Integer result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+
+        key = 6;
+        value = 2;
+        expResult=null;
+        result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+
+        key = 6;
+        value = 3;
+        expResult=2;
+        result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testAddThreeElementsTwoKeysLeft() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        Integer expResult = null;
+        Integer result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+
+        key = 4;
+        value = 2;
+        expResult=null;
+        result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+
+        key = 4;
+        value = 3;
+        expResult=2;
+        result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testAddThreeElementsThreeKeysLeftAndRIght() {
+        Integer key = 5;
+        Integer value = 1;
+        avlTree = new AVLTree();
+        Integer expResult = null;
+        Integer result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+
+        key = 4;
+        value = 2;
+        expResult=null;
+        result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+
+        key = 6;
+        value = 3;
+        expResult=null;
+        result = avlTree.add(key, value);
+        assertEquals(expResult, result);
+    }
+
+
+
+    /**
+     * Test of isEmpty method, of class AVLTree.
+     */
+    @Test
+    public void testIsEmptyTrue() {
+        boolean isEmpty= avlTree.isEmpty();
+        assertTrue(isEmpty);
+    }
+    @Test
+    public void testIsEmptyOneElement() {
+        Integer key = 4;
+        Integer value = 2;
+        avlTree.add(key, value);
+        boolean isEmpty= avlTree.isEmpty();
+        assertFalse(isEmpty);
+    }
 
 
 } 
